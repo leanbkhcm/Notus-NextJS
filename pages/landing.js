16e86bdc242e3efa7b97ea7d57138202b326029f
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-07-19 09:44:36 +0700

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -193,13 +193,7 @@ export default function Landing() {
 
           <div className="container mx-auto px-4">
             <div className="items-center flex flex-wrap">
-              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                <img
-                  alt="..."
-                  className="max-w-full rounded-lg shadow-lg"
-                  src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                />
-              </div>
+              
               <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
                 <div className="md:pr-12">
                   <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-200">
@@ -257,6 +251,15 @@ export default function Landing() {
                   </ul>
                 </div>
               </div>
+
+
+              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                              <img
+                                alt="..."
+                                className="max-w-full rounded-lg shadow-lg"
+                                src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                              />
+                            </div>
             </div>
           </div>
         </section>
