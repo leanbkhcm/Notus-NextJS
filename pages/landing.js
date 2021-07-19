61a096c6f8deb556183ec43a802d0d1c1b2e8d48
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-07-19 14:43:11 +0700

    hello Vietnam 123 landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -29,7 +29,7 @@ export default function Landing({data}) {
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                 <div className="pr-12">
                   <h1 className="text-white font-semibold text-5xl">
-                   hello Vietnam
+                   hello Vietnam 123
                     {data.element_count}
                   </h1>
                   <p className="mt-4 text-lg text-blueGray-200">
