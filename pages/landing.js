686f2acf805255b66cfabddd4a7b2ff0f6132fa9
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-07-19 16:38:10 +0700

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -29,13 +29,14 @@ export default function Landing({data}) {
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                 <div className="pr-12">
                   <h1 className="text-white font-semibold text-5xl">
-                   hello Vietnam VN
+                   hello Vietnam VN 123
                     {data.element_count}
                     
 
                   </h1>
                   <p className="mt-4 text-lg text-blueGray-200">
-                    
+                    {data.near_earth_objects.'2015-09-08'[0].name}
+                    hello vn 132
                     This is a simple example of a Landing Page you can build
                     using Notus NextJS. It features multiple CSS components
                     based on the Tailwind CSS design system.
