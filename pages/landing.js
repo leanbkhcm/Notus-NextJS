cc3f9a5ed93147674dfadbe83f505559312c4d9c
unknown <letranhungan1989@gmail.com>
2021-07-22 14:06:26 +0700

    change api

--- pages/landing.js
+++ pages/landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
-import api from './api/unsplash';
-import {getCuratedPhotos} from "../api/unsplash"
+//import api from './api/unsplash';
+import {getCuratedPhotos} from './api/unsplash';
 
 //import { useAlert } from 'react-alert';
 // components
