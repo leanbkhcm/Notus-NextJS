1d9ad0edbc4a93f2d3d0e442c6b82dece9b01cab
an.lth@sitv.com.vn <an.lth@sitv.com.vn>
2021-07-23 16:20:27 +0700

    at kimlong v2

--- pages/landing.js
+++ pages/landing.js
@@ -47,8 +47,8 @@ export default function Landing() {
                     Your story starts with us.
                   </h1>
                   <p className="mt-4 text-lg text-blueGray-200">
-                    This is a simple example of a Landing Page you can build
-                    using Notus NextJS. It features multiple CSS components
+                    Anle learning NextJs.
+					It features multiple CSS components
                     based on the Tailwind CSS design system.
                   </p>
                 </div>
