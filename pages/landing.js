9ff21f82e95dcfd09216bf52c4e4adc5e98942f4
an.lth@sitv.com.vn <an.lth@sitv.com.vn>
2021-07-23 16:40:37 +0700

    add image canada

--- pages/landing.js
+++ pages/landing.js
@@ -64,7 +64,7 @@ export default function Landing() {
             className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
             style={{ transform: "translateZ(0)" }}
           >
-            {/* <svg
+            <svg
               className="absolute bottom-0 overflow-hidden"
               xmlns="http://www.w3.org/2000/svg"
               preserveAspectRatio="none"
@@ -77,7 +77,7 @@ export default function Landing() {
                 className="text-blueGray-200 fill-current"
                 points="2560 0 2560 100 0 100"
               ></polygon>
-            </svg> */}
+            </svg>
           </div>
         </div>
         {/* end cover pic */}
@@ -188,7 +188,7 @@ export default function Landing() {
                     className="w-full align-middle rounded-t-lg"
                   />
                   <blockquote className="relative p-8 mb-4">
-                    {/* <svg
+                    <svg
                       preserveAspectRatio="none"
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 583 95"
@@ -198,7 +198,7 @@ export default function Landing() {
                         points="-30,95 583,95 583,65"
                         className="text-blueGray-700 fill-current"
                       ></polygon>
-                    </svg> */}
+                    </svg>
                     <h4 className="text-xl font-bold text-white">
                       Top Notch Services
                     </h4>
@@ -224,7 +224,7 @@ export default function Landing() {
             className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
             style={{ transform: "translateZ(0)" }}
           >
-            {/* <svg
+            <svg
               className="absolute bottom-0 overflow-hidden"
               xmlns="http://www.w3.org/2000/svg"
               preserveAspectRatio="none"
@@ -237,77 +237,60 @@ export default function Landing() {
                 className="text-white fill-current"
                 points="2560 0 2560 100 0 100"
               ></polygon>
-            </svg> */}
+            </svg>
           </div>
 
+
+
+
+
           <div className="container mx-auto px-4">
-            <div className="items-center flex flex-wrap">
-              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                <img
-                  alt="..."
-                  className="max-w-full rounded-lg shadow-lg"
-                  src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                />
-              </div>
-              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-                <div className="md:pr-12">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-200">
-                    <i className="fas fa-rocket text-xl"></i>
+
+
+
+          {data.map(function(item){
+            return (
+              <div className="items-center flex flex-wrap">
+                <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                  <img
+                    alt="..."
+                    className="max-w-full rounded-lg shadow-lg"
+                    src={item.urls.full}
+                  />
+                </div>
+                <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                  <div className="md:pr-12">
+                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-200">
+                      <i className="fas fa-rocket text-xl"></i>
+                    </div>
+                    <h3 className="text-3xl font-semibold">{item.alt_description}</h3>
+              
+                    {/* <ul className="list-none mt-6">
+                      <li className="py-2">
+                        <div className="flex items-center">
+                          <div>
+                            <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                              <i className="fas fa-fingerprint"></i>
+                            </span>
+                          </div>
+                          <div>
+                            <h4 className="text-blueGray-500">
+                              Carefully crafted components
+                            </h4>
+                          </div>
+                        </div>
+                      </li>
+                    </ul> */}
                   </div>
-                  <h3 className="text-3xl font-semibold">A growing company</h3>
-                  <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                    The extension comes with three pre-built pages to help you
-                    get started faster. You can change the text and images and
-                    you're good to go.
-                  </p>
-                  <ul className="list-none mt-6">
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fas fa-fingerprint"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Carefully crafted components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fab fa-html5"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Amazing page examples
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="far fa-paper-plane"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Dynamic components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
                 </div>
               </div>
-            </div>
+            )
+            })} 
           </div>
+
+
+
+
         </section>
 
         <section className="pt-20 pb-48">
@@ -472,7 +455,7 @@ export default function Landing() {
             className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
             style={{ transform: "translateZ(0)" }}
           >
-            {/* <svg
+            <svg
               className="absolute bottom-0 overflow-hidden"
               xmlns="http://www.w3.org/2000/svg"
               preserveAspectRatio="none"
@@ -485,7 +468,7 @@ export default function Landing() {
                 className="text-blueGray-800 fill-current"
                 points="2560 0 2560 100 0 100"
               ></polygon>
-            </svg> */}
+            </svg>
           </div>
 
           <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
