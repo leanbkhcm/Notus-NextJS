0635d20bc5ddfc6fa9d0771e3fe88cd14a676702
an.lth@sitv.com.vn <an.lth@sitv.com.vn>
2021-07-23 16:51:55 +0700

    add image canada add getServer 10 images

--- pages/api/unsplash.js
+++ pages/api/unsplash.js
@@ -1,5 +1,5 @@
 const API_CLIENTID = process.env.UNSPLASH_ACCESS_KEY;
-const API_URL = `https://api.unsplash.com/search/photos?page=1&per_page=20&client_id=kKUaTLCb1_-n3V4w79Clo-3m8pKstx_-6EHX1eM_QBI`;
+const API_URL = `https://api.unsplash.com/search/photos?page=1&per_page=10&client_id=kKUaTLCb1_-n3V4w79Clo-3m8pKstx_-6EHX1eM_QBI`;
 //const API_URL = `https://api.unsplash.com/search/photos?page=1&per_page=20&client_id=${API_CLIENTID}`;
 
 export default {
