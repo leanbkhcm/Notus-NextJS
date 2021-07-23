29f0483a5b10f7edae382f39e4c5d577df3318df
unknown <letranhungan1989@gmail.com>
2021-07-23 09:47:00 +0700

    add responsive image

--- pages/landing.js
+++ pages/landing.js
@@ -164,77 +164,42 @@ export default function Landing({data}) {
             </svg>
           </div>
 
+
+     
+
+
+              
           <div className="container mx-auto px-4">
-            <div className="items-center flex flex-wrap">
-              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                <img
-                  alt="..."
-                  className="max-w-full rounded-lg shadow-lg"
-                  src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                />
-              </div>
+            {data.map(function(item){
+                return (
 
 
+              <div className="items-center flex flex-wrap">
+                <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                  <img
+                    alt="..."
+                    className="max-w-full rounded-lg shadow-lg"
+                    src={item.urls.full}
+                  />
+                </div>
 
-              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-                <div className="md:pr-12">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-200">
-                    <i className="fas fa-rocket text-xl"></i>
+                <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                  <div className="md:pr-12">
+                    <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                    {item.alt_description}
+                    </p>                  
                   </div>
-                  <h3 className="text-3xl font-semibold">A growing company 123</h3>
-                  <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                    The extension comes with three pre-built pages to help you
-                    get started faster. You can change the text and images and
-                    you're good to go.
-                  </p>
-                  <ul className="list-none mt-6">
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fas fa-fingerprint"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Carefully crafted components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fab fa-html5"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Amazing page examples
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="far fa-paper-plane"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Dynamic components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
                 </div>
               </div>
-            </div>
+
+
+            )
+            })}  
+
           </div>
+
+
+
         </section>
 
         <section className="pt-20 pb-48">
@@ -259,7 +224,7 @@ export default function Landing({data}) {
               {/* {data.sort((a, b) => b.height - a.height)} */}
 
 
-              {data.map(function(item){
+              {/* {data.map(function(item){
                 return (
                 <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
                   <div className="px-6">
@@ -280,7 +245,7 @@ export default function Landing({data}) {
                   </div>
                   </div>
                 )
-              })}  
+              })}   */}
             </div>
 
 
